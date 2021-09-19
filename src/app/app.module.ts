04db6a9cbfd83ebb16343a3cebf1c4f83ca85121
GhazanJs <ghazan.usman@aksa-sds.com>
2021-09-19 05:12:36 -0700

    Added some basic folders about posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreatePostsComponent } from './posts/create-posts/create-posts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule } from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CreatePostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
